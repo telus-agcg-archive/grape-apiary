3b39f7b9fdf3f52b45a28a9e86a73bea36f553f7
John Allen <john@threedogconsulting.com>
2014-02-08 20:47:11 -0500

    Some spec related cleanup

--- spec/support/config_context.rb
+++ spec/support/config_context.rb
@@ -5,24 +5,10 @@ shared_context 'configuration' do
   let(:resource_exclusion) { [:admin, :swagger_docs] }
 
   let(:request_headers) do
-    [
-      { 'Accept-Charset' => 'utf-8' },
-      { 'Connection'     => 'keep-alive' },
-      { 'Content-Type'   => 'application/json' }
-    ]
+    [{ 'Accept-Charset' => 'utf-8' }]
   end
 
   let(:response_headers) do
-    [
-      { 'Content-Length' => '21685' },
-      { 'Connection'     => 'keep-alive' },
-      { 'Content-Type'   => 'application/json' }
-    ]
-  end
-
-  let(:app) do
-    def app
-      SampleApi
-    end
+    [{ 'Connection'     => 'keep-alive' }]
   end
 end
